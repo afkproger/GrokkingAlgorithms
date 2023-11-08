f845118ae47c6c0be33efd189526ce19da6bbca3
afkproger <126967895+afkproger@users.noreply.github.com>
2023-11-09 01:52:45 +0300

    Реализовал быструю сортировку и написал комменьарии к коду

--- Algorithms/src/Sotrings/Main.java
+++ Algorithms/src/Sotrings/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         QuickSort quickSort = new QuickSort();
-        ArrayList<Integer> integers = new ArrayList<>(List.of(3,5,3,2,5,6,8,9,5));
-        //System.out.println(quickSort.sort(integers));
+        ArrayList<Integer> integers = new ArrayList<>(List.of(10,5,3,2));
+        System.out.println(quickSort.quicksort(integers));
     }
 }
