473721c98adb456a0ec1343742207099839b57dc
afkproger <126967895+afkproger@users.noreply.github.com>
2023-11-11 11:26:04 +0300

    Реализация быстрой сортировки

--- Algorithms/src/Sotrings/QuickSort.java
+++ Algorithms/src/Sotrings/QuickSort.java
@@ -26,38 +26,33 @@ import java.util.stream.Stream;
 
 
 public class QuickSort {
-    public List<Integer> quicksort(List<Integer> list) {
-        if (list.size() < 2) {
+    public List<Integer> quicksort (List<Integer> list){
+        if (list.size() < 2){
             return list;
-            // Базовый случай
-        } else {
-            int pivot = list.get(0); // это наша опорная точка
-            List<Integer> smallestPivot = new ArrayList<>();  // подмассив для элементов меньше опорной точки
-            List<Integer> biggestPivot = new ArrayList<>();   // подмассив для элементов больше опорной точки
+        }else {
+            int indexForPivot = list.size()/2;
+            int pivot = list.get(indexForPivot);
+            List<Integer> smallestPivot = new ArrayList<>();
+            List<Integer> biggestPivot = new ArrayList<>();
 
-            //цикл для отбора подмассивов, нужно не забыть что мы идём от 1 элемента, т.к.опорная точка - нулевой эллемент
-            for (int i = 1; i < list.size(); i++) {
-                if (list.get(i) <= pivot) {
+            for (int i = 0; i < list.size(); i++) {
+                if(i == indexForPivot){
+                    continue;
+                }
+                if(list.get(i) <= pivot){
                     smallestPivot.add(list.get(i));
-                } else {
+                }else {
                     biggestPivot.add(list.get(i));
                 }
             }
-            /**
-             * Почему не получалось до этого?
-             * Я забывал присвоить smallest and biggest новые значения, т.е. отсортированные массивы
-             * Как это выглядело:quicksort(smallestPivot);  quicksort(biggestPivot);
-             * Короче, как обычно, накринжевал и сидел довольным
-             */
 
             smallestPivot = quicksort(smallestPivot);
             biggestPivot = quicksort(biggestPivot);
 
-            List<Integer> sortingList = new ArrayList<>(smallestPivot);
-            sortingList.add(pivot);
-            sortingList.addAll(biggestPivot);
-
-            return sortingList;
+            List<Integer> subList = new ArrayList<>(smallestPivot);
+            subList.add(pivot);
+            subList.addAll(biggestPivot);
+            return subList;
         }
     }
 }
